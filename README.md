b4597c37cbe04e1c3136038abf56084d8ccc230e
gssan <gssify@gmail.com>
2016-10-24 10:31:35 +0800

    增加export-wildcard依赖

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-定制化打包工具，依靠 webpack 打包，需要使用 babel 
+YD定制化打包工具
+
+使用场景：仅打包 JavaScript 文件，并且支持 IE8
+
+生产依赖：
+- regular
+- regular-ui
+- es5-shim
+
+开发依赖：
+- webpack
+- babel-laoder
+- rgl-loader
+- export-wildcard-loader
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,23 @@ module.exports = {
     entry: './index.js',
     output: {
         path: __dirname,
-        filename: "rgui.min.js"
+        filename: 'rgui.min.js',
+        library: 'R',
+        libraryTarget: 'umd'
+    },
+    externals: {
+        'regularjs': {
+            root: "Regular",
+            amd: "Regular",
+            commonjs: "regularjs",
+            commonjs2: "regularjs"
+        }
     },
     module: {
         loaders: [
             { test: /\.rgl$/, loader: 'rgl' },
-            { test: /\.js$/, loader: 'babel'}
+            { test: /\.js$/, exclude: /node_modules\/(?!rgui-)/,  loader: 'babel' },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'export-wildcard' }
         ]
     }
 }
